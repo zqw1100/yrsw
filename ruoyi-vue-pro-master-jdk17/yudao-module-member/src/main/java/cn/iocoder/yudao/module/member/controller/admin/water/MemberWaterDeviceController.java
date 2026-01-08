ec81f1984424b09e10994dc1853389b9090188e4
zqw1100 <38744103+zqw1100@users.noreply.github.com>
2026-01-08 13:26:00 +0800

    feat: integrate water meter valve and upload APIs

--- ruoyi-vue-pro-master-jdk17/yudao-module-member/src/main/java/cn/iocoder/yudao/module/member/controller/admin/water/MemberWaterDeviceController.java
+++ ruoyi-vue-pro-master-jdk17/yudao-module-member/src/main/java/cn/iocoder/yudao/module/member/controller/admin/water/MemberWaterDeviceController.java
@@ -2,8 +2,11 @@ package cn.iocoder.yudao.module.member.controller.admin.water;
 
 import cn.iocoder.yudao.framework.common.pojo.CommonResult;
 import cn.iocoder.yudao.framework.common.pojo.PageResult;
+import cn.iocoder.yudao.module.member.controller.admin.water.vo.MemberWaterDeviceChangeReqVO;
 import cn.iocoder.yudao.module.member.controller.admin.water.vo.MemberWaterDevicePageReqVO;
 import cn.iocoder.yudao.module.member.controller.admin.water.vo.MemberWaterDeviceRespVO;
+import cn.iocoder.yudao.module.member.controller.admin.water.vo.MemberWaterDeviceUploadModeReqVO;
+import cn.iocoder.yudao.module.member.controller.admin.water.vo.MemberWaterDeviceValveReqVO;
 import cn.iocoder.yudao.module.member.service.water.MemberWaterDeviceService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -12,6 +15,8 @@ import jakarta.validation.Valid;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -41,5 +46,27 @@ public class MemberWaterDeviceController {
     public CommonResult<Boolean> refreshDevice(@RequestParam("id") Long id) {
         deviceService.refreshDevice(id);
         return success(true);
+    }
+
+    @PostMapping("/valve")
+    @Operation(summary = "阀门操作")
+    @PreAuthorize("@ss.hasPermission('member:water-device:update')")
+    public CommonResult<Boolean> operateValve(@Valid @RequestBody MemberWaterDeviceValveReqVO reqVO) {
+        return success(deviceService.operateValve(reqVO.getDeviceNo(), reqVO.getValveStatus()));
+    }
+
+    @PostMapping("/change-device")
+    @Operation(summary = "换表")
+    @PreAuthorize("@ss.hasPermission('member:water-device:update')")
+    public CommonResult<Boolean> changeDevice(@Valid @RequestBody MemberWaterDeviceChangeReqVO reqVO) {
+        return success(deviceService.changeDevice(reqVO.getOriginalDeviceCode(), reqVO.getNewDeviceCode(),
+                reqVO.getOriginalTotalData()));
+    }
+
+    @PostMapping("/upload-mode")
+    @Operation(summary = "设置设备上传周期")
+    @PreAuthorize("@ss.hasPermission('member:water-device:update')")
+    public CommonResult<Boolean> setUploadMode(@Valid @RequestBody MemberWaterDeviceUploadModeReqVO reqVO) {
+        return success(deviceService.setUploadMode(reqVO.getDeviceCode(), reqVO.getUploadType(), reqVO.getValue()));
     }
 }
